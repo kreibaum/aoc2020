14d81f9511a9d3e6570e3db00fc85c7763c585c1
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2020-12-01 06:27:45 +0100

    Solve day 1

--- main.jl
+++ main.jl
@@ -2,4 +2,37 @@
 
 # Day 1
 
-@assert 5 == @show 2 + 3
+# Before you leave, the Elves in accounting just need you to fix your expense
+# report (your puzzle input); apparently, something isn't quite adding up.
+
+day_1_sample = [1721, 979, 366, 299, 675, 1456];
+day_1_input = parse.(Int, readlines(open("input-01")))
+
+function match2020(lines; total = 2020)
+    expense_set = Set(lines);
+    for line in lines
+        if (total - line) in expense_set
+            return line * (total - line)
+        end
+    end
+    return -1
+end
+
+@assert 514579 == @show match2020(day_1_sample)
+@assert 1010299 == @show match2020(day_1_input)
+
+## Part 2
+# In your expense report, what is the product of the three entries that sum to 2020?
+
+function match2020three(lines)
+    for line in lines
+        product = match2020(lines; total = 2020 - line)
+        if product >= 0
+            return product * line
+        end
+    end
+    return -1
+end
+
+@assert 241861950 == @show match2020three(day_1_sample)
+@assert 42140160 == @show match2020three(day_1_input)
